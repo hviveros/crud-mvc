56e9cab4a77e2191e371ff644549ebfac023e599
Humberto Viveros <humberto.viverosrios@gmail.com>
2020-01-24 00:24:00 -0300

    Renombrado archivos de la carpeta /paginas

---
+++ views/paginas/eliminar.php
@@ -0,0 +1,25 @@
+
+	<main role="main" class="container">
+
+		<div class="starter-template">
+			<h1>CRUD sencillo con PHP - MVC - PDO</h1>
+			<hr>
+			<div class="col-md-6 offset-3">
+				<form class="text-left">
+					<div class="form-group">
+						<label for="nombre">Nombre</label>
+						<input type="text" class="form-control" id="nombre" aria-describedby="nombreHelp" value="Mark" disabled>
+						<small id="nombreHelp" class="form-text text-muted">Ingrese el nombre completo del cliente.</small>
+					</div>
+					<div class="form-group">
+						<label for="email">E-mail</label>
+						<input type="email" class="form-control" id="email" aria-describedby="emailHelp" value="mark@mark.py" disabled>
+						<small id="emailHelp" class="form-text text-muted">Ingrese el correo electronico del cliente.</small>
+					</div>
+					<button type="submit" class="btn btn-danger">Eliminar registro</button>
+				</form>
+			</div>
+		</div>
+
+	</main><!-- /.container -->
+	
