5cab3ae3870eb3c3ac3c00f7f7a8a07dde6dc121
Humberto Viveros <humberto.viverosrios@gmail.com>
2020-01-24 00:10:43 -0300

    ClienteModel: agregado el navbar en los metodos index, insertar, editar, eliminar

--- controllers/ClienteController.php
+++ controllers/ClienteController.php
@@ -7,24 +7,28 @@ class ClienteController {
 	#estableciendo las vistas
 	public function inicio() {
         require_once('./views/includes/cabecera.php');
+        require_once('./views/includes/navbar.php');
         require_once('./views/paginas/inicio.php');
         require_once('./views/includes/pie.php');
 	}
 
 	public function insertar() {
         require_once('./views/includes/cabecera.php');
+        require_once('./views/includes/navbar.php');
         require_once('./views/paginas/insertar.php');
         require_once('./views/includes/pie.php');
 	}
 
 	public function editar() {
         require_once('./views/includes/cabecera.php');
+        require_once('./views/includes/navbar.php');
         require_once('./views/paginas/editar.php');
         require_once('./views/includes/pie.php');
 	}
 
 	public function eliminar() {
         require_once('./views/includes/cabecera.php');
+        require_once('./views/includes/navbar.php');
         require_once('./views/paginas/eliminar.php');
         require_once('./views/includes/pie.php');
 	}
