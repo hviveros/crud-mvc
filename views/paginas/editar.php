109d176c19d67895b7bf1f434b678444859391f9
Humberto Viveros <humberto.viverosrios@gmail.com>
2020-01-25 11:55:55 -0300

    Archivo editar.php-> llamado del controlador, se obtiene el id del registro a editar y con este, se llena el formulario, una vez enviado el formulario, se crea el array para su envio al controlador

--- views/paginas/editar.php
+++ views/paginas/editar.php
@@ -1,22 +1,49 @@
+<?php
 
+    require_once 'controllers/ClienteController.php';
+    $objeto = new ClienteController();
+
+    /*obtener el valor 'id' de la url para buscar en la bd
+    y posteriormente llenar el formulario*/
+    if (isset($_GET['id'])) {
+	    $idCliente = $_GET['id'];
+    	$cliente = $objeto->obtenerCliente($idCliente);
+    }
+
+    if (isset($_POST['editar'])) {
+    	$datos = array(
+			'id'   		=> $_POST['id'],
+			'nombre'   	=> $_POST['nombre'],
+			'email'    	=> $_POST['email'],
+		);
+		$objeto->editarCliente($datos);
+    }
+
+?>
 	<main role="main" class="container">
 
 		<div class="starter-template">
 			<h1>CRUD sencillo con PHP - MVC - PDO</h1>
 			<hr>
 			<div class="col-md-6 offset-3">
-				<form class="text-left">
-					<div class="form-group">
-						<label for="nombre">Nombre</label>
-						<input type="text" class="form-control" id="nombre" aria-describedby="nombreHelp" value="Mark">
-						<small id="nombreHelp" class="form-text text-muted">Ingrese el nombre completo del cliente.</small>
-					</div>
-					<div class="form-group">
-						<label for="email">E-mail</label>
-						<input type="email" class="form-control" id="email" aria-describedby="emailHelp" value="mark@mark.py">
-						<small id="emailHelp" class="form-text text-muted">Ingrese el correo electronico del cliente.</small>
-					</div>
-					<button type="submit" class="btn btn-info">Editar registro</button>
+				<form action="index.php?page=editar" method="POST" name="editarForm" id="editarForm" class="text-left">
+					<?php
+						if (!empty($cliente)) {
+							foreach ($cliente as $r) { 
+					?>
+						<input type="hidden" name="id" value="<?= $r['id']; ?>">
+						<div class="form-group">
+							<label for="nombre">Nombre</label>
+							<input type="text" id="nombre" name="nombre" class="form-control" aria-describedby="nombreHelp" value="<?= $r['nombre']; ?>">
+							<small id="nombreHelp" class="form-text text-muted">Ingrese el nombre completo del cliente.</small>
+						</div>
+						<div class="form-group">
+							<label for="email">E-mail</label>
+							<input type="email" id="email" name="email" class="form-control" aria-describedby="emailHelp" value="<?= $r['email']; ?>">
+							<small id="emailHelp" class="form-text text-muted">Ingrese el correo electronico del cliente.</small>
+						</div>
+						<button type="submit" name="editar" class="btn btn-info">Editar registro</button>
+					<?php } } ?>
 				</form>
 			</div>
 		</div>
