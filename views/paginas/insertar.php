d2bfc249ba5c141d36625f45a13454cdaf365ff8
Humberto Viveros <humberto.viverosrios@gmail.com>
2020-01-25 00:11:53 -0300

    Formulario Insertar

--- views/paginas/insertar.php
+++ views/paginas/insertar.php
@@ -1,22 +1,36 @@
+<?php 
 
+require_once 'controllers/ClienteController.php';
+
+$objeto = new ClienteController();
+
+if (isset($_POST['registrar'])) {
+	$datos = array(
+		'nombre'   => $_POST['nombre'],
+		'email'    => $_POST['email'],
+	);
+	$objeto->insertarCliente($datos);
+}
+
+?>
 	<main role="main" class="container">
 
 		<div class="starter-template">
 			<h1>CRUD sencillo con PHP - MVC - PDO</h1>
 			<hr>
 			<div class="col-md-6 offset-3">
-				<form class="text-left">
+				<form action="index.php?page=insertar" method="POST" name="registroForm" id="registroForm" class="text-left">
 					<div class="form-group">
 						<label for="nombre">Nombre</label>
-						<input type="text" class="form-control" id="nombre" aria-describedby="nombreHelp">
+						<input type="text" id="nombre" name="nombre" class="form-control" aria-describedby="nombreHelp">
 						<small id="nombreHelp" class="form-text text-muted">Ingrese el nombre completo del cliente.</small>
 					</div>
 					<div class="form-group">
 						<label for="email">E-mail</label>
-						<input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+						<input type="email" id="email" name="email" class="form-control" aria-describedby="emailHelp">
 						<small id="emailHelp" class="form-text text-muted">Ingrese el correo electronico del cliente.</small>
 					</div>
-					<button type="submit" class="btn btn-primary">Guardar registro</button>
+					<button type="submit" name="registrar" class="btn btn-primary">Guardar registro</button>
 				</form>
 			</div>
 		</div>
