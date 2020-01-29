a3f08033c508e3714ba177db26739d74864b10d7
Humberto Viveros <humberto.viverosrios@gmail.com>
2020-01-28 23:02:08 -0300

    Preparacion del formulario paginas/eliminar.php

--- views/paginas/eliminar.php
+++ views/paginas/eliminar.php
@@ -1,22 +1,43 @@
+<?php
 
+    require_once 'controllers/ClienteController.php';
+    $objeto = new ClienteController();
+
+    /*obtener el valor 'id' de la url para buscar en la bd
+    y posteriormente llenar el formulario*/
+    if (isset($_GET['id'])) {
+	    $idCliente = $_GET['id'];
+    	$cliente = $objeto->obtenerCliente($idCliente);
+    }
+
+    if (isset($_POST['eliminar'])) {
+    	$id = $_POST['id'];
+		$objeto->eliminarCliente($id);
+    }
+
+?>
 	<main role="main" class="container">
 
 		<div class="starter-template">
 			<h1>CRUD sencillo con PHP - MVC - PDO</h1>
 			<hr>
 			<div class="col-md-6 offset-3">
-				<form class="text-left">
-					<div class="form-group">
-						<label for="nombre">Nombre</label>
-						<input type="text" class="form-control" id="nombre" aria-describedby="nombreHelp" value="Mark" disabled>
-						<small id="nombreHelp" class="form-text text-muted">Ingrese el nombre completo del cliente.</small>
-					</div>
-					<div class="form-group">
-						<label for="email">E-mail</label>
-						<input type="email" class="form-control" id="email" aria-describedby="emailHelp" value="mark@mark.py" disabled>
-						<small id="emailHelp" class="form-text text-muted">Ingrese el correo electronico del cliente.</small>
-					</div>
-					<button type="submit" class="btn btn-danger">Eliminar registro</button>
+				<form action="index.php?page=eliminar" method="POST" name="eliminarForm" id="eliminarForm" class="text-left">
+					<?php
+						if (!empty($cliente)) {
+							foreach ($cliente as $r) { 
+					?>
+						<input type="hidden" name="id" value="<?= $r['id']; ?>">
+						<div class="form-group">
+							<label for="nombre">Nombre</label>
+							<input type="text" id="nombre" name="nombre" class="form-control" aria-describedby="nombreHelp" value="<?= $r['nombre']; ?>" disabled>
+						</div>
+						<div class="form-group">
+							<label for="email">E-mail</label>
+							<input type="email" id="email" name="email" class="form-control" aria-describedby="emailHelp" value="<?= $r['email']; ?>" disabled>
+						</div>
+						<button type="submit" name="eliminar" class="btn btn-danger">Eliminar registro</button>
+					<?php } } ?>
 				</form>
 			</div>
 		</div>
